8769e7414c5f1917d0a03649ab83a42c7f209e84
Vladimir Beliakov <vladimirred456@gmail.com>
2020-02-16 01:15:03 +0300

    Add printable characters.

--- Parser/GlobalConstants.cs
+++ Parser/GlobalConstants.cs
@@ -11,15 +11,23 @@ namespace Parser
 		private const char LF = '\u000A';
 		private const char CR = '\u000D';
 
+		private const string BasicLatinSubset = "\u0020-\u007E";
+
 		private const char DEL = '\u007F';
 
 		private const string C1ControlBlock = "\u0080-\u009F";
 		private const char NEL = '\u0085';
 
+		private const string LatinSupplementToHangulJamo = "\u00A0-\uD7FF";
+
 		private const string SurrogateBlock = "\uD800-\uDFFF\uFFFE\uFFFF";
 
+		private const string PrivateUseAreaToSpecialsBeginning = "\uE000-\uFFFD";
+
+		private const string LinearBSyllabaryToSupplementaryPrivateUseArea = "\u10000-\u10FFFF";
+
 		#endregion
-		
+
 		public const int CharSequenceLength = 100;
 
 		public static readonly string CommentRegex =
@@ -32,6 +40,14 @@ namespace Parser
 				.Concat(C1ControlBlock.Except(new[] { NEL }))
 				.Concat(new[] { DEL })
 				.Concat(SurrogateBlock)
+				.ToString();
+
+		public static readonly string PrintableChars =
+			new[] { TAB, LF, CR, NEL }
+				.Concat(BasicLatinSubset)
+				.Concat(LatinSupplementToHangulJamo)
+				.Concat(PrivateUseAreaToSpecialsBeginning)
+				.Concat(LinearBSyllabaryToSupplementaryPrivateUseArea)
 				.ToString();
 	}
 }
