0257b9d53c6abfccf91fcbe3293080fafaeedc4a
Vladimir Beliakov <vladimirred456@gmail.com>
2020-12-12 20:04:52 +0300

    Refactor SimpleStyles (delete unnecessary string formating).

--- src/Processor/FlowStyles/SimpleStyles.cs
+++ src/Processor/FlowStyles/SimpleStyles.cs
@@ -3,9 +3,9 @@ namespace Processor.FlowStyles
 	public static class SimpleStyles
 	{
 		public static string AliasNode =
-			$"{BasicStructures.SeparateInLine}" +
-			$"{Characters.Alias}{BasicStructures.NodeTags.AnchorName}" +
-			$"{BasicStructures.Break}";
+			BasicStructures.SeparateInLine +
+			Characters.Alias + BasicStructures.NodeTags.AnchorName +
+			BasicStructures.Break;
 
 		// Note: There can be empty nodes, specifically, node's property and content both can be empty.
 		// Empty nodes usually get resolved to null.
