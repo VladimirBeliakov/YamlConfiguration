63443846cb58337d9778caf3a3c78ad074f22499
Vladimir Beliakov <vladimirred456@gmail.com>
2021-06-12 19:58:27 +0300

    Refactor RegexPattern to struct.

--- src/Processor/RegexPattern.cs
+++ src/Processor/RegexPattern.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace YamlConfiguration.Processor
 {
-	public class RegexPattern : IEquatable<RegexPattern>
+	public readonly struct RegexPattern : IEquatable<RegexPattern>
 	{
 		private readonly string _regexValue;
 
@@ -21,7 +21,7 @@ namespace YamlConfiguration.Processor
 			if (pattern._regexValue.Length == 1)
 				return pattern._regexValue.Single();
 
-			throw new InvalidCastException($"Can't cast multi char pattern {pattern} to one char.");
+			throw new InvalidCastException($"Can't cast the multi char pattern '{pattern}' to one char.");
 		}
 
 		public static explicit operator RegexPattern(string rawValue) => new RegexPattern(rawValue);
@@ -29,21 +29,16 @@ namespace YamlConfiguration.Processor
 		public static explicit operator RegexPattern(char rawValue) => new RegexPattern(rawValue.ToString());
 
 		public static RegexPattern operator +(RegexPattern pattern1, RegexPattern pattern2) =>
-			new RegexPattern(pattern1._regexValue + pattern2._regexValue);
+			new RegexPattern($"{pattern1._regexValue}{pattern2._regexValue}");
+
+		public bool Equals(RegexPattern other)
+		{
+			return _regexValue == other._regexValue;
+		}
 
 		public override bool Equals(object? obj)
 		{
-			if (ReferenceEquals(null, obj)) return false;
-			if (ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
-			return Equals((RegexPattern) obj);
-		}
-
-		public bool Equals(RegexPattern? other)
-		{
-			if (ReferenceEquals(null, other)) return false;
-			if (ReferenceEquals(this, other)) return true;
-			return _regexValue == other._regexValue;
+			return obj is RegexPattern other && Equals(other);
 		}
 
 		public override int GetHashCode()
@@ -51,14 +46,14 @@ namespace YamlConfiguration.Processor
 			return _regexValue.GetHashCode();
 		}
 
-		public static bool operator ==(RegexPattern? left, RegexPattern? right)
+		public static bool operator ==(RegexPattern left, RegexPattern right)
 		{
-			return Equals(left, right);
+			return left._regexValue == right._regexValue;
 		}
 
-		public static bool operator !=(RegexPattern? left, RegexPattern? right)
+		public static bool operator !=(RegexPattern left, RegexPattern right)
 		{
-			return !Equals(left, right);
+			return left._regexValue != right._regexValue;
 		}
 	}
 }
